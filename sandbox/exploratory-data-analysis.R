bde6f8b36ce8b77eb504c73225d28128cc3302e6
andrewpbray <andrew.bray@gmail.com>
2017-07-19 14:03:53 -0600

    add fit all models, coefficient clustering, and map prelims

--- sandbox/exploratory-data-analysis.R
+++ sandbox/exploratory-data-analysis.R
@@ -1,4 +1,7 @@
 library(GGally)
+library(tidyverse)
+library(purrr)
+library(broom)
 
 # load data
 load(url("https://www.dropbox.com/s/v0cal2s54mizlfo/urban.rda?dl=1"))
@@ -6,6 +9,9 @@ load(url("https://www.dropbox.com/s/v0cal2s54mizlfo/urban.rda?dl=1"))
 # find urban gridcells
 is_urban <- (rowSums(!is.na(urban[, , "Tu"])) > 0)
 
+
+#================#
+# Study 3 cells only
 # make plots for 3 random urban gridcells
 set.seed(304)
 n <- 3
@@ -18,17 +24,55 @@ for (i in 1:n) {
   dev.off()
 }
 
-
-# model a few gridcells
-
 # slice off cells of interest and scale vars
 urban_subset <- urban[which(is_urban)[rand_cells], , ] %>%
   alply(1)
 urban_subset <- map(urban_subset, ~apply(.x, MARGIN = 2, scale))
 
-
-# fit several SLRs
+# fit models
 m <- map(urban_subset, ~lm(Tu ~ ., data = as.data.frame(.x)))
+
+
+#================#
+# Fit all models
+
+# Data processing
+all_jans <- seq(1, dim(urban)[2], by = 12)
+urban_list <- urban[is_urban,
+                    all_jans,
+                    c("Ld", "LiqPrecip", "P", "PCO2", "Q", "Sd", "T", "Tu")] %>%
+  alply(1) %>%
+  map(~apply(.x, MARGIN = 2, scale))
+
+# Fit Models
+model_list <- map(urban_list, ~lm(Tu ~ ., data = as.data.frame(.x)))
+
+# Extract coefficients
+coef_list <- map(model_list, ~tidy(.x)$estimate)
+coef_df <- data.frame(matrix(unlist(coef_list), nrow = length(urban_list)))
+names(coef_df) <- c("intercept", names(coef(m1)[-1]))
+ggpairs(coef_df)
+
+# k-means clustering
+df <- data.frame(k = 2:10, withinss = rep(NA, 9))
+for (i in 1:9) {
+  df[i, "withinss"] <- kmeans(coef_df, centers = i + 1, iter.max = 15)$tot.withinss
+}
+ggplot(df, aes(x = k, y = withinss)) +
+  geom_point() +
+  geom_line()
+
+# hierarchical clustering
+subsamp <- sample_n(as.data.frame(scale(coef_df)), size = 200) %>%
+  dist()
+hc <- hclust(subsamp)
+plot(hc, xlab = "", ylab = "", sub = "", main = "Complete Linkage", cex = .6)
+
+
+
+
+
+
 
 # cross-validation
 k <- 5
@@ -36,6 +80,41 @@ u <- urban_subset[[1]]
 for (i in 1:k) {
   test <- u
 }
+
+
+# make map of urban areas in North America
+library(ggmap)
+
+theme_map <- function(...) {
+  theme_minimal() +
+    theme(
+      text = element_text(family = "Ubuntu Regular", color = "#22211d"),
+      axis.line = element_blank(),
+      axis.text.x = element_blank(),
+      axis.text.y = element_blank(),
+      axis.ticks = element_blank(),
+      axis.title.x = element_blank(),
+      axis.title.y = element_blank(),
+      # panel.grid.minor = element_line(color = "#ebebe5", size = 0.2),
+      panel.grid.major = element_line(color = "#ebebe5", size = 0.2),
+      panel.grid.minor = element_blank(),
+      plot.background = element_rect(fill = "#f5f5f2", color = NA),
+      panel.background = element_rect(fill = "#f5f5f2", color = NA),
+      legend.background = element_rect(fill = "#f5f5f2", color = NA),
+      panel.border = element_blank(),
+      ...
+    )
+}
+
+north_am <- c(left = -128, bottom = 12, right = -65, top = 55)
+map <- get_stamenmap(north_am, zoom = 5, maptype = "terrain-background")
+p1 <- ggmap(map) +
+  theme_map()
+
+is_urban <- (rowSums(!is.na(urban[, , "Tu"])) > 0)
+is_urban_mat <- matrix(is_urban, nrow = 288)
+
+
 
 
 #
