0d46bb592b415a9edd793badca4d20b95e44baf5
andrewpbray <andrew.bray@gmail.com>
2017-07-20 23:17:25 -0600

    add prediction component

--- sandbox/predict.R
+++ sandbox/predict.R
@@ -40,16 +40,32 @@ is_urban <- (rowSums(!is.na(urban[, , "Tu"])) > 0)
 # Add month variable
 d <- t(array(rep(rep(1:12, times = 95), times = dim(urban)[1]),
              dim = c(1140, sum(is_urban))))
-urban_cube <- abind(d, urban[is_urban, , c("Ld", "LiqPrecip", "P", "Q", "Sd", "Tu")])
-dimnames(urban_cube)[[3]][1] <- "month"
-urban_list <- alply(urban_cube, 1)
+urban_cube <- abind(d, urban[is_urban, , ])
 
 # grab PCO2 from original run
-dimnames(urban)[[3]] <- c("Tref", "Q", "P", "Ld", "Sd", "LiqPrecip")
+urban_cube <- abind(urban_train[is_urban, 13:1152, "PCO2"], urban_cube)
 
-is_urban <- (rowSums(!is.na(urban[, , "Tu"])) > 0)
-urban_list <- urban[is_urban, , c("Ld", "LiqPrecip", "P", "PCO2", "Q", "Sd", "T", "Tu")] %>%k
-  alply(1) %>%
-  map(~apply(.x, MARGIN = 2, scale))
+dimnames(urban_cube)[[3]] <- c("PCO2", "month", "Tref", "Q", "P", "Ld" , "Sd" , "LiqPrecip")
+
+# prepare as newdata
+urban_cube <- urban_cube[ , , c("month", "Ld", "LiqPrecip", "P", "PCO2", "Q", "Sd")]
+newdata <- urban_cube %>%
+  alply(1, as.data.frame)
+
+# predict
+p1 <- predict(m_full_list[[1]], newdata = newdata[[1]])
+pred_list <- purrr::map2(.x = m_full_list, .y = newdata, ~predict(.x, newdata = .y))
+pred_cube <- unlist(pred_list) %>%
+  array(dim = c(288, 192, 1140))
+
+# output
+#devtools::use_data(pred_cube)
+
+
+
+
+
+
+
 
 
